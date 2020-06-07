ae827d2cde35bdf339f54d9eec1960a3745617e3
FluffleFox <44639764+FluffleFox@users.noreply.github.com>
2020-06-07 13:16:15 +0200

    SkyUpdate

---
+++ Assets/Scripts/CloundMovment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloundMovment : MonoBehaviour
+{
+    public float wind;
+    Material mat;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetTextureOffset("_MainTex", Vector2.right * wind * Time.realtimeSinceStartup);
+    }
+}
