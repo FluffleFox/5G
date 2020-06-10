bb0a82993aaa3bc2bfa4ede4c4489f1aeadfb190
FluffleFox <44639764+FluffleFox@users.noreply.github.com>
2020-06-10 20:31:02 +0200

    HeliSceneUpdate

---
+++ Assets/Scripts/DontDestroyableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyableObject : MonoBehaviour
+{
+    public static DontDestroyableObject instance;
+    void Start()
+    {
+        //po prostu kurwa zrobić z tego singleton
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/HeliSceneMenagment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeliSceneMenagment : MonoBehaviour
+{
+    public Transform[] moveOnDropObject;
+    public float[] moveOnDropSpeed;
+    public int dropTime = 30;
+    float dropSpeed = 0.01f;
+    bool ready = false;
+   public void Ready()
+   {
+        if (!ready)
+        {
+            ready = true;
+            StartCoroutine(Drop()); 
+        }
+   }
+
+    IEnumerator Drop()
+    {
+        for(int i=0; i<dropTime; i++)
+        {
+            for(int j=0; j<moveOnDropObject.Length; j++)
+            {
+                moveOnDropObject[j].transform.position += Vector3.up * i * 9.81f * dropSpeed * moveOnDropSpeed[j];
+                yield return new WaitForSecondsRealtime(dropSpeed);
+            }
+        }
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
