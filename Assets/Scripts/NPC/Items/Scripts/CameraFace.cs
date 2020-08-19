f921c3cb4761155f742e7754ac50d39f50148730
FluffleFox <piotr.grzebielucha@gmail.com>
2020-08-19 14:03:22 +0200

    ScaleUpdate

--- Assets/Scripts/NPC/Items/Scripts/CameraFace.cs
+++ Assets/Scripts/NPC/Items/Scripts/CameraFace.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CameraFace : MonoBehaviour
 {
-    
+    const float tangent30 = 1.7320508075688772935274463415059f; //1/tg(30)
+
+
     void Update()
     {
         transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position);
+        float h = Camera.main.transform.position.z - transform.position.z;
+        float d = 2 * h * tangent30*0.005f;
+        transform.localScale = Vector3.one * d;
     }
 }
