50a70338cfb762f3656ec644246bdacbce697196
FluffleFox <piotr.grzebielucha@gmail.com>
2020-08-27 22:31:44 +0200

    ZamrazajacyNPC

---
+++ Assets/Scripts/NPC/Items/Scripts/FrozenEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FrozenEffect : Item
+{
+    float speed;
+    public float defrostTime=1;
+
+    private void Start()
+    {
+        ResetChance();
+    }
+
+    public override void ItemAction()
+    {
+        base.ItemAction();
+        speed = transform.parent.GetComponent<Movment>().movmentSpeed;
+        transform.parent.GetComponent<Movment>().movmentSpeed = 0;
+        model.transform.localScale = Vector3.one;
+    }
+
+    private void Update()
+    {
+        if (model.activeSelf)
+        {
+            model.transform.localScale -= Vector3.up * Time.deltaTime/defrostTime;
+            if (model.transform.localScale.y <= 0.0f)
+            {
+                transform.parent.GetComponent<Movment>().movmentSpeed = speed;
+                LastFrameAction();
+            }
+        }
+    }
+
+    public override void LastFrameAction()
+    {
+        base.LastFrameAction();
+        if (speed > 0.5f)
+        {
+            transform.parent.GetComponent<Movment>().movmentSpeed = speed;
+        }
+        else
+        {
+            transform.parent.GetComponent<Movment>().movmentSpeed = Random.Range(0.8f, 1.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/Items/Scripts/FrozenExplosion.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class FrozenExplosion : Item
+{
+    public LayerMask mask;
+    bool canIExplode = false;
+
+    void Start()
+    {
+        GeneralGameMenager.instance.SwitchToNormal.AddListener(SetChance);
+        GeneralGameMenager.instance.QuitingRage.AddListener(ResetChance);
+        ResetChance();
+        canIExplode = false;
+    }
+
+    public override void ItemAction()
+    {
+        base.ItemAction();
+        transform.parent.gameObject.GetComponent<NPC_ControlScript>().AddScore(5);
+        canIExplode = false;
+    }
+
+    private void Update()
+    {
+        if (model.activeSelf)
+        {
+            model.transform.position = transform.parent.GetChild(0).GetChild(1).position;
+        }
+    }
+
+    public override void ItemHitAction()
+    {
+        base.ItemHitAction();
+        if (model.activeSelf && canIExplode)
+        {
+            foreach (Collider k in Physics.OverlapSphere(transform.position, 2.5f, mask))
+            {
+                k.transform.Find("FrozenEffect").GetComponent<FrozenEffect>().ItemAction();
+            }
+            model.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HPCheeck")
+        {
+            canIExplode = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "HPCheeck")
+        {
+            canIExplode = false;
+        }
+    }
+
+    public override void LastFrameAction()
+    {
+        base.LastFrameAction();
+    }
+}
